6210e406a843d8a8b96ef5647c75f333f071a8ba
Fnixya <62068603+Fnixya@users.noreply.github.com>
2024-05-12 01:32:05 +0200

    me han quedado las graficas de puta madre

--- main.py
+++ main.py
@@ -10,41 +10,68 @@ config = {
     'plot': True
 }
 
-VARS = ['Age', 'IncomeLevel', 'Assets', 'Amount', 'Job', 'History', 'Risk']
-PLOT_ROWS = 2
-PLOT_COLS = 4
 
 class FuzzySetPlot:
     def __init__(self, fuzzyRisks: classes.FuzzySetsDict, fuzzyVars: classes.FuzzySetsDict):
         self.fuzzySets = fuzzyRisks
         self.fuzzySets.update(fuzzyVars)
-        # self.fuzzyVars = fuzzyVars
-        
-        self.fig, self.axis = plt.subplots(PLOT_ROWS, PLOT_COLS)
-        self.fig.subplots_adjust(bottom=0.2)
+
+        # Constants        
+        self.VARS = ['Age', 'IncomeLevel', 'Assets', 'Amount', 'Job', 'History', 'Risk']
+        self.LINE_STYLES = ['-g', '-y', '-r', '-k']
+        self.PLOT_ROWS = 2
+        self.PLOT_COLS = 4
+
+        # Create plot
+        self.fig, self.axis = plt.subplots(self.PLOT_ROWS, self.PLOT_COLS, figsize=(12, 5))
+        self.fig.subplots_adjust(left=0.1, right=0.95, bottom=0.1, top=0.9, wspace=0.4, hspace=0.3)
         self.axis[0][0].grid(True) 
-        # self.ax.set_aspect('equal')
+        # self.axis.set_aspect('equal')
+
+        # Create axis for Risk
+        gs = self.axis[0, -1].get_gridspec()
+        for ax in self.axis[0:, -1]:
+            ax.remove()
+        self.axRisk = self.fig.add_subplot(gs[0:, -1])
+
         
 
     def plot(self) -> None:
-        line_styles = ['-g', '-y', '-r', '-m']
-        self.graphs = [[]] * len(VARS)
-        self.count = [0] * len(VARS)
+        self.graphs = [[] for _ in range(7)]
+        self.counts = [0] * len(self.VARS)
+        self.labels = [[] for _ in range(7)]
 
         # Plots graph for each fuzzy set in the dictionary
         for fuzzySet in self.fuzzySets:
-            i = VARS.index(self.fuzzySets[fuzzySet].var)
-            graph = self.axis[i%PLOT_ROWS][i//PLOT_ROWS].plot(
-                self.fuzzySets[fuzzySet].x, 
-                self.fuzzySets[fuzzySet].y,    
-                line_styles[self.count[i]]
-            ),
-            self.graphs[i].append(graph)
-            self.count[i] += 1
+            i = self.VARS.index(self.fuzzySets[fuzzySet].var)
+            if i == 6:
+                graph = self.axRisk.plot(
+                    self.fuzzySets[fuzzySet].x, 
+                    self.fuzzySets[fuzzySet].y,    
+                    self.LINE_STYLES[self.counts[i]],
+                    label=self.fuzzySets[fuzzySet].label
+                )
+            else:
+                graph = self.axis[i%self.PLOT_ROWS][i//self.PLOT_ROWS].plot(
+                    self.fuzzySets[fuzzySet].x, 
+                    self.fuzzySets[fuzzySet].y,    
+                    self.LINE_STYLES[self.counts[i]],
+                    label=self.fuzzySets[fuzzySet].label
+                )
 
-        for i in range(0, 7):
-            self.axis[i%PLOT_ROWS][i//PLOT_ROWS].set_xlabel(VARS[i])
-            self.axis[i%PLOT_ROWS][i//PLOT_ROWS].set_ylabel("Degree of truth")
+            self.graphs[i].append(graph)
+            self.labels[i].append(self.fuzzySets[fuzzySet].label)
+            self.counts[i] += 1
+
+        for i in range(0, 6):
+            ax = self.axis[i%self.PLOT_ROWS][i//self.PLOT_ROWS]
+            ax.set_xlabel(self.VARS[i])
+            ax.set_ylabel("Degree of truth")
+            ax.legend(self.labels[i], loc='lower right', fontsize='xx-small')
+
+        self.axRisk.set_xlabel(self.VARS[6])
+        self.axRisk.set_ylabel("Degree of truth")
+        self.axRisk.legend(self.labels[6], loc='lower right', fontsize='xx-small')
 
     def render(self) -> None:
         plt.show()
